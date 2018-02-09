f0276123c798266e26a21e4f59de1ed49f76b5ea
bernard169 <bernard@tourneur.org>
2018-02-09 17:22:46 +0100

    add factoriel computation to webb app

--- server.py
+++ server.py
@@ -4,10 +4,33 @@
 # Version: February 8, 2018
 
 import cherrypy
+import utils
+import re
 
-class WebApp:
+class WebApp():
     @cherrypy.expose
     def index(self):
-        return 'Hello World!'
+        return 
+            '''
+            <h2>Coucou</h2>
+            <form method="post" action="computeFactoriel" title="Factoriel">
+                <p>
+                    <label>Entrez un nombre positif : </label> <input type="number" name="inputNumber"/>
+                </p>
+                <p>
+                    <label>Réponse :</label>
+                    {{content}}
+                </p>
+            </form>
+            '''
+    
+    def computeFactoriel (self, inputNumber):
+        pattern = "^\d+$"
+        p = re.compile (pattern)
+        if p.match (inputNumber) is not None :
+            content = str (utils.fact (int (inputNumber))
+        else :
+            content = "Veuillez entrer un nombre positif"
+        return {'content': content}
 
 cherrypy.quickstart(WebApp(), '', 'server.conf')
